22007208355eafc858946f96ee749554b31f96cd
marc19555 <marc.dolder@viacesi.fr>
2026-04-07 08:59:46 +0400

    Front Marc première étape

--- SportManager/Views/MainScreen.xaml.cs
+++ SportManager/Views/MainScreen.xaml.cs
@@ -5,5 +5,23 @@ public partial class MainScreen : ContentPage
 	public MainScreen()
 	{
 		InitializeComponent();
+		
 	}
+	
+	//mauvais root mais c'est pour voir la page de recrutement
+	private async void OnGestionEquipeClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync(nameof(RecrutementPage));
+	}
+	//mauvais root mais c'est pour voir la page de modification de match
+	private async void OnJouerMatchClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync(nameof(ModificationDetailJoueurView));
+	}
+	//mauvais root mais tkt
+	private async void OnHistoriqueClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync(nameof(RecrutementPage));
+	}
+
 }
--- SportManager/Views/ModificationDetailJoueurView.xaml.cs
+++ SportManager/Views/ModificationDetailJoueurView.xaml.cs
@@ -1,3 +1,5 @@
+using Microcharts;
+using SkiaSharp;
 namespace SportManager.Views;
 
 public partial class ModificationDetailJoueurView : ContentPage
@@ -5,5 +7,37 @@ public partial class ModificationDetailJoueurView : ContentPage
 	public ModificationDetailJoueurView()
 	{
 		InitializeComponent();
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var entries = new[]
+        {
+            new ChartEntry(80) { Label = "Vitesse",   Color = SKColor.Parse("#f28c57") },
+            new ChartEntry(60) { Label = "Endurance", Color = SKColor.Parse("#f28c57") },
+            new ChartEntry(90) { Label = "Technique", Color = SKColor.Parse("#f28c57") },
+            new ChartEntry(25) { Label = "Force",     Color = SKColor.Parse("#f28c57") },
+        };
+
+        radarChart.Chart = new RadarChart
+        {
+            Entries = entries,
+            MinValue = 0,
+            MaxValue = 100,
+            LineSize = 2,
+            PointMode = PointMode.Circle,
+            PointSize = 12,
+            LabelTextSize = 22,
+            LabelColor = SKColor.Parse("#ffffff"),
+            BackgroundColor = SKColor.Parse("#3a3a3a"),
+            BorderLineSize = 1
+        };
+    }
+
+	private void OnValiderClicked(object sender, EventArgs e)
+	{
+		// TODO: logique de validation
 	}
 }
