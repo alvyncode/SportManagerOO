e215a7e19939228daef53a131c776a6019f3724f
marc19555 <marc.dolder@viacesi.fr>
2026-04-09 19:25:42 +0400

    préparation merge

--- SportManager/AppShell.xaml.cs
+++ SportManager/AppShell.xaml.cs
@@ -5,7 +5,5 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-		Routing.RegisterRoute(nameof(Views.RecrutementPage), typeof(Views.RecrutementPage));
-		Routing.RegisterRoute(nameof(Views.ModificationDetailJoueurView), typeof(Views.ModificationDetailJoueurView));
 	}
 }
--- SportManager/Views/MainScreen.xaml.cs
+++ SportManager/Views/MainScreen.xaml.cs
@@ -7,21 +7,4 @@ public partial class MainScreen : ContentPage
 		InitializeComponent();
 		
 	}
-	
-	//mauvais root mais c'est pour voir la page de recrutement
-	private async void OnGestionEquipeClicked(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync(nameof(RecrutementPage));
-	}
-	//mauvais root mais c'est pour voir la page de modification de match
-	private async void OnJouerMatchClicked(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync(nameof(ModificationDetailJoueurView));
-	}
-	//mauvais root mais tkt
-	private async void OnHistoriqueClicked(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync(nameof(RecrutementPage));
-	}
-
 }
